577a62385705dc295161449aa89d321ddb08b221
Gauge <Gauger16@gmail.com>
2014-11-13 17:15:35 -0800

    Finalized v0.0.1

--- main.js
+++ main.js
@@ -1,114 +1,124 @@
+var path = require('path');
+var fs = require('fs');
+
+var lastdepth = -1; 
+var visited = [];
+
 module.exports = function (runner) {
-  var passes = 0;
-  var failures = 0;
+  var status = {
+    pass: 0,
+    fail: 0,
+    pending: 0
+  };
 
-  var hierarchy = [];
-  var title = '';
-  var ptitle = '';
-  var suiteID = 0;
-  var parentID = 0;
-  var errorcount = 0;
-
-  var table = '<table cellspacing="0" cellpadding="0">';
+  // where all the data is stored
+  var doc = '';
+  var suites = '';
 
   runner.on('start', function() {
-    
-    var text = 
-    '<html>' +
-      '<head>' +
-        '<style type="text/css">' +
-          'body {background-color: SeaShell}' +
-          'table {border-collapse: collapse; margin-left: auto; margin-right: auto; width: 70%; background-color: #E7EDF5; table-layout: fixed;}'+
-          'th, td {border: 1px solid black; word-break: break-word;}' +
-          '.indent {width:10px;}' +
-          '.duration {width: 50px;}' +
-          '.title  {width: auto;}' +
-          '.passState {color: white; background-color: DarkGreen; width: 45px; text-align: right;}' +
-          '.failState {color: white; background-color: DarkRed; width: 45px; text-align: right;}' +
-          '.pass {background-color: #33CC66;}' +
-          '.fail {background-color: #E0A7A7;}' +
-          '.failDetail {width: auto; word-wrap: break-word;}' +
-          '.hypertext {text-decoration: underline; color: blue;}' +
-          '.endStyle {text-align: center; background-color: #E7EDF5; width: 150px; margin-left: auto; margin-right: auto;}' +
-        '</style>' +
-        '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>' +
-        '<script>var showHide = function(id,pid) { var element = $("."+id); var pelement = $("."+pid); if (pelement.css("display") == "none") { element.toggle(false); element.click(); } else { element.toggle(); if (element.css("display") == "none") {element.click();}}}</script>' +
-      '</head><body>';
-
-    console.log(text);
+    var value = fs.readFileSync(path.join(__dirname, 'docs/header.html'), "utf8");
+    doc = value;
+    console.log(value);
   });
 
 
   runner.on('end', function() {
-
-    var text = '<div class="endStyle">'+
-      '<div>Total: '+(passes+failures)+'</div>'+
-      '<div style="color: DarkGreen;">Passed: ' + passes + '</div>' +
-      '<div style="color: DarkRed;">Failed: ' + failures + '</div>' +
-    '</div></body></html>';
-    
-    console.log(text);
+    var value = fs.readFileSync(path.join(__dirname, 'docs/footer.html'), "utf8");
+    doc += value;
+    console.log(value);
   });
 
   runner.on('suite', function(suite) {
-
-    title = removeSpecialChars(suite.title);
-    ptitle = (suite.parent) ? removeSpecialChars(suite.parent.title) : '';
-    errorcount = 0;
-    
-    if (title != '' && ptitle == ''){
-      suiteID = 0;
-      parentID = 0;
-      hierarchy = [title];
-    } else {
-      parentID = hierarchy.indexOf(ptitle);
-      suiteID = parentID+1;
-      hierarchy.splice(suiteID, hierarchy.length);
-      hierarchy.push(title);
+    // calculate nesting level
+    var depth = 0;
+    var object = suite;
+    while (!object.root) {
+      depth++;
+      object = object.parent;
     }
-
-    if (title != '') {
-      var text = table+'<tr onclick="showHide(\''+title+suiteID+'\', \''+ptitle+parentID+'\')" class="'+ptitle+parentID+' section">'+
-        addIndentation(suiteID) +
-        '<td style="width: auto" class="hypertext">'+ suite.title + '</td>'  +
-      '</tr></table>';
-
-      console.log(text);
-    }
+    suite.depth = depth;
   });
 
-  runner.on('pass', function(test) {
-    passes++;
-    var data = table+'<tr class="'+title+suiteID+' pass" >' +
-      addIndentation(suiteID+1) +
-      '<td class="duration">'+ test.duration + 'ms</td>'+
-      '<td class="title">'+ test.title + '</td>' +
-      '<td class="passState">'+ test.state + '</td>' +
-    '</tr></table>';
+  runner.on('suite end', function(suite) {
+    if (suite.root) return; // do not do anything if its the root
 
-    console.log(data);
+    var depth = suite.depth;
+    if (lastdepth == -1) lastdepth = depth;
+    setupVisitedArray(depth);
 
+    var errorcount = 0;
+    var title = removeSpecialChars(suite.title);
+    var ptitle = removeSpecialChars(suite.parent.title);
+
+    var tests = '';
+    suite.tests.forEach(function(test, index, array) {
+      var parentReference = title+depth;
+      var state = test.state
+
+      if (state == 'passed') status.pass++;
+      if (state == 'failed') status.fail++;
+      if (state == 'undefined') status.pending++; 
+
+      if (state == 'failed') {
+        errorcount++;
+        tests += '<table cellspacing="0" cellpadding="0">'+
+          '<tr onclick="showHide(\''+parentReference+'err'+errorcount+'\', \''+parentReference+'\')" class="'+parentReference+' failed">' +
+            addIndentation(depth+1) + // tests reside one step deaper than its parent suite
+            '<td class="duration">'+ test.duration + 'ms</td>'+
+            '<td class="title hypertext">'+ test.title + '</td>' +
+            '<td class="'+state+'State">'+ state + '</td>' +
+          '</tr>'+
+        '</table>';
+
+        tests += '<table cellspacing="0" cellpadding="0">' +
+          '<tr class="'+parentReference+'err'+errorcount+' failed">' +
+            addIndentation(depth+2) +
+            '<td class="failDetail">'+
+              '<pre style="font-family: \'Courier New\', Courier, monospace;">'+
+                '<code>' + test.err + '</code>'+
+              '</pre>'+
+            '</td>'+
+          '</table>';
+      
+      } else {
+        tests += '<table cellspacing="0" cellpadding="0">'+
+          '<tr class="'+parentReference+' '+state+'" >' +
+            addIndentation(depth+1) + // tests reside one step deaper than its parent suite
+            '<td class="duration">'+ test.duration + 'ms</td>'+
+            '<td class="title">'+ test.title + '</td>' +
+            '<td class="'+state+'State">'+ state + '</td>' +
+          '</tr>'+
+        '</table>';
+      }
+    });
+
+    var text = '';
+    text += '<table cellspacing="0" cellpadding="0">'+
+      '<tr onclick="showHide(\''+title+depth+'\', \''+ptitle+(depth-1)+'\')" class="'+ptitle+(depth-1)+'">'+
+        addIndentation(depth) +
+        '<td style="width: auto" class="hypertext">'+ suite.title + '</td>'  +
+      '</tr></table>';
+    text += tests;
+
+    suites = (visited[depth] == 1)? suites+text : text+suites;
+    visited[depth] = 1;
+    lastdepth = depth;
+
+    if (suite.depth == 1) { // reset the stored suites for the next heirarchy
+      doc += suites;
+      console.log(suites);
+      suites = '';
+      return;
+    }
   });
 
   runner.on('fail', function(test, err){
-    failures++;
-    errorcount++;
-    var data = table+'<tr onclick="showHide(\''+title+suiteID+'err'+errorcount+'\', \''+title+suiteID+'\')" class="'+title+suiteID+' fail">' +
-      addIndentation(suiteID+1) +
-      '<td class="duration"></td>' +
-      '<td class="title hypertext">'+ test.title + '</td>' +
-      '<td class="failState">'+ test.state + '</td>' +
-    '</tr></table>';
-
-    data += table+'<tr class="'+title+suiteID+'err'+errorcount+' fail">' +
-      addIndentation(suiteID+2) +
-      '<td class="failDetail"><pre style="font-family: \'Courier New\', Courier, monospace;"><code>' + err + '</code></pre></td></table>';
-
-    console.log(data);
+      test.err = err;
   });
 }
 
 var addIndentation = function(indent) {     
+  indent = indent-1;
   var data = '';
   for (var i=0; i<indent; i++) {
     
@@ -132,9 +142,12 @@ var removeSpecialChars = function(text) {
   return text;
 }
 
-module.exports.test = function() {
-  var fs = require('fs');
-  var doc = fs.readFileSync('./header.html');
-  // add other data
-  console.log(doc);
+var setupVisitedArray = function(depth) {
+  while(visited.length <= depth) {
+    visited.push(0);
+  }
+
+  for(var i=lastdepth; i<visited.length; i++){
+    visited[i] = 0;
+  }
 }
